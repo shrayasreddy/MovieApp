b176995c16ddf43f2b15f81b1c3c5fb8df7986c3
shrayasreddy <shrayas.india@gmail.com>
2021-10-18 00:44:28 +0530

    added animation to the logo

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import '../header/Header.css';
 import Logo from '../../assets/logo.svg';
-//import "../../common/header/Header";
+//import { keyframes } from "styled-components";
+
 
 const Header = function(){
+  /*  var spin = keyframes`
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+`;*/
 const headerStyle = {
     height:'35px',
    textAlign: 'left',
    background: '#ff7f7f',
-   position: 'absolute', top: 9, left: 9, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center',
+   position: 'fixed', top: 9, left: 9, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center',
+   animation: 'spin 8s linear infinite',
 };
+ 
+
     return(
        <div className='header'>
-        <img src={Logo} alt="logo" style={headerStyle}></img>
+      
+        <img  src={Logo} alt="logo" style={headerStyle} className='logoImage' ></img>
+        
        </div>
     ) 
 }
