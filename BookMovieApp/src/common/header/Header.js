0633465481ba666de6743034101fb3914e936ccf
shrayasreddy <shrayas.india@gmail.com>
2021-10-19 10:29:21 +0530

    added modal and created component folder for it

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../header/Header.css';
 import Logo from '../../assets/logo.svg';
-//import { keyframes } from "styled-components";
+import  Modal  from '../components/Modal';
+
 
 
 const Header = function(){
-  /*  var spin = keyframes`
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-`;*/
+  
 const headerStyle = {
     height:'35px',
    textAlign: 'left',
@@ -16,14 +14,22 @@ const headerStyle = {
    position: 'fixed', top: 9, left: 9, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center',
    animation: 'spin 8s linear infinite',
 };
- 
+const [modalOpen, setModalOpen] = useState(false);
 
     return(
+    
        <div className='header'>
-      
         <img  src={Logo} alt="logo" style={headerStyle} className='logoImage' ></img>
-        
-       </div>
+        <div id='button'>
+        <button variant="contained" href="#contained-buttons" className="openModalBtn"
+        onClick={() => {
+          setModalOpen(true);
+        }} >Login</button>
+        {modalOpen && <Modal setOpenModal={setModalOpen} />}
+        </div>
+        </div>
+       
+     
     ) 
 }
 
