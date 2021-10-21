9329781ef9c11a4798f87b915aebe50bc15d58cd
shrayasreddy <shrayas.india@gmail.com>
2021-10-21 20:09:35 +0530

    added modal login and register layout

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -1,36 +1,136 @@
-import React,{useState} from 'react';
-import '../header/Header.css';
+import React, { Component } from 'react';
+import "./Header.css"
 import Logo from '../../assets/logo.svg';
-import  Modal  from '../components/Modal';
+import Button from '@mui/material/Button';
+import ReactModal from 'react-modal';
+import Tabs from '@mui/material/Tabs';
+import TabPanel from '../TabPane';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import FormGroup from '@material-ui/core/FormGroup';
 
 
+import Input from '@mui/material/Input';
 
-const Header = function(){
-  
-const headerStyle = {
-    height:'35px',
-   textAlign: 'left',
-   background: '#ff7f7f',
-   position: 'fixed', top: 9, left: 9, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center',
-   animation: 'spin 8s linear infinite',
+import FormHelperText from '@mui/material/FormHelperText';
+console.log((window.location.href.indexOf("movie") > -1))
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
 };
-const [modalOpen, setModalOpen] = useState(false);
 
-    return(
-    
-       <div className='header'>
-        <img  src={Logo} alt="logo" style={headerStyle} className='logoImage' ></img>
-        <div id='button'>
-        <button variant="contained" href="#contained-buttons" className="openModalBtn"
-        onClick={() => {
-          setModalOpen(true);
-        }} >Login</button>
-        {modalOpen && <Modal setOpenModal={setModalOpen} />}
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      modalopen: false,
+      modalTab: 0,
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      contactNo: ''
+
+    }
+  }
+  setOpenModal = () => {
+   
+    this.setState({ modalopen: !this.state.modalopen })
+  }
+
+  handleTabChange = (e, value) => {
+   
+    this.setState({ modalTab: value })
+  }
+  render() {
+    return (
+      <div>
+
+
+        <div class="HeaderBar">
+          <img className="Logo" src={Logo} />
+          <div class="header-right">
+            {(window.location.href.indexOf("movie/") > -1) ? <Button className="BookShowBtn" variant="contained" color="primary" > Book Show</Button> : ''}
+            <Button variant="contained" color="inherit" onClick={this.setOpenModal} > Login</Button>
+
+
+          </div>
         </div>
-        </div>
-       
-     
-    ) 
+        <ReactModal
+          style={customStyles}
+          isOpen={this.state.modalopen}
+        >
+          <div style={{ textAlign: "end" }}>
+            <Button variant="outlined" onClick={this.setOpenModal}>x</Button>
+          </div>
+          <hr></hr>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={this.state.modalTab} onChange={this.handleTabChange} aria-label="basic tabs example">
+              <Tab value={0} label="LOGIN" />
+              <Tab value={1} label="REGISTER" />
+            </Tabs>
+          </Box>
+
+          <TabPanel value={this.state.modalTab} index={0} >
+            <FormGroup>
+              <FormControl required={this.state.firstName == '' ? true : false} color="primary">
+                <InputLabel htmlFor="username">UserName</InputLabel>
+                <Input id="username" aria-describedby="my-helper-text" />
+              </FormControl>
+              <FormControl required={this.state.password == '' ? true : false}  >
+                <InputLabel htmlFor="Password">Password</InputLabel>
+                <Input id="Password" aria-describedby="my-helper-text" />
+              </FormControl>
+              <Button variant="contained">LOGIN</Button>
+            </FormGroup>
+          </TabPanel>
+
+
+          <TabPanel value={this.state.modalTab} index={1}>
+            <FormGroup>
+              <FormControl required={this.state.firstName == '' ? true : false} color="primary">
+                <InputLabel htmlFor="firstName">First Name</InputLabel>
+                <Input id="firstName" aria-describedby="my-helper-text" />
+                <FormHelperText error={this.state.firstName == '' ? true : false} id="component-error-text">required</FormHelperText>
+              </FormControl>
+              <FormControl required={this.state.lastName == '' ? true : false}  >
+                <InputLabel htmlFor="Password">Password</InputLabel>
+                <Input id="Password" aria-describedby="my-helper-text" />
+                <FormHelperText error={this.state.lastName == '' ? true : false} id="password-text">required</FormHelperText>
+              </FormControl>
+              <FormControl required={this.state.firstName == '' ? true : false} color="primary">
+                <InputLabel htmlFor="username">UserName</InputLabel>
+                <Input id="username" aria-describedby="my-helper-text" />
+                <FormHelperText error={this.state.firstName == '' ? true : false} id="component-error-text">required</FormHelperText>
+              </FormControl>
+              <FormControl required={this.state.firstName == '' ? true : false}  >
+                <InputLabel htmlFor="Password">Password</InputLabel>
+                <Input id="Password" aria-describedby="my-helper-text" />
+                <FormHelperText error={this.state.firstName == '' ? true : false} id="component-error-text">required</FormHelperText>
+              </FormControl>
+              <FormControl required={this.state.firstName == '' ? true : false} color="primary">
+                <InputLabel htmlFor="username">UserName</InputLabel>
+                <Input id="username" aria-describedby="my-helper-text" />
+                <FormHelperText error={this.state.firstName == '' ? true : false} id="component-error-text">required</FormHelperText>
+              </FormControl>
+              <Button variant="contained">REGISTER</Button>
+            </FormGroup>
+          </TabPanel>
+
+        </ReactModal>
+
+      </div>
+    )
+  }
 }
 
-export default Header;
+export default Header
